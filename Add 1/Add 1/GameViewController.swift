5ab950b73b11408e244641fc56b2322b68d1aa37
emmaxs <emmaxs@users.noreply.github.com>
2020-01-13 10:23:11 -0500

    First commit, ui working

---
+++ Add 1/Add 1/GameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Add 1
+//
+//  Created by Emma Sampugnaro on 1/13/20.
+//  Copyright © 2020 Emma Sampugnaro. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
